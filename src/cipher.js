60d6a389a8f30c20d59fa9492d88d6c95ae66643
floresdani <florescaceresdaniela@gmail.com>
2018-05-31 00:34:47 -0400

    agregando código de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,14 @@
 window.cipher = {
   encode: (offset, string) => {
-		/* Acá va tu código */
-  }
+    /* Acá va tu código */
+  let cipherText = ""; //string vacio que voy a recibir
+  let i = 0; //indice es cero
+  for(i = 0; i < string.length; i++){ //que inicie en el indice con valor cero, que se detenga recorriendo la longitud del string e incremente en 1 al itinerar.
+    let letterEncode = ((string.charCodeAt(i) - 65 + parseInt(offset)) % 26)+ 65;
+    cipherText += String.fromCharCode(letterEncode).toLowerCase();
+  } 
+    document.getElementById("inputText").innerHTML = cipherText;
+  },
 
   decode: (offset, string ) => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-/* Acá va tu código */
-
 window.onload = () => {
 // almacenando valor del texto a codificar 
 const btnCipher = document.getElementById('btn-cipher');
 btnCipher.addEventListener("click", () => {
-   let codeText = document.getElementById("inputText").value;
-   let position = document.getElementById("offset").value;
-   window.cipher.encode(position,codeText);
+  let codeText = document.getElementById("inputText").value;
+  let position = document.getElementById("offset").value;
+  cipher.encode(position,codeText);
 });
 // almacenando valor del texto a decodificar 
 const btnDecipher = document.getElementById('btn-decipher');
 btnDecipher.addEventListener("click", () => {
   let decodeText = document.getElementById("inputText").value;
-  let document.getElementById("offset").value;
-  window.cipher.enconde(position,decodeText);
+  document.getElementById("offset").value;
+  cipher.enconde(position,decodeText);
 });
 }
 
